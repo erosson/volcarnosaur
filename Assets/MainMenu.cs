f96b1ed54f1ccc2ca4f436179cb6111ee68ab3d3
Evan Rosson <github@erosson.org>
2014-04-25 20:55:13 -0700

    crude mainmenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		// TODO pick a real name
+		GUILayout.Label("LD48");
+		if (GUILayout.Button ("Play")) {
+			Play();
+        }
+		if (GUILayout.Button ("Website")) {
+			Application.OpenURL("http://erosson.org/games/LD48");
+		}
+        if (!Application.isWebPlayer && !Application.isEditor) {
+			if (GUILayout.Button ("Exit")) {
+				Application.Quit();
+            }
+        }
+        GUILayout.EndArea();
+	}
+
+	private void Play() {
+		Application.LoadLevel("Game");
+	}
+}
