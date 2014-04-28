271f2028b75b428c7f00cbda1b9a0d1a0d9f6239
Evan Rosson <github@erosson.org>
2014-04-28 06:52:08 -0700

    Squashed 'Assets/erossonUnityUtils/' changes from 2be838f..cdb7aaa
    cdb7aaa readme formatting
    b48e748 gitignore includes unity defaults
    6991125 more subtree directions
    ff5a251 more subtree directions
    483972e readme with submodule instructions
    0589e97 refactoring. move debugutil to utils package
    
    git-subtree-dir: Assets/erossonUnityUtils
    git-subtree-split: cdb7aaad8addc51919a1c07c543b344f9e2e90bd

---
+++ DebugUtil.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugUtil {
+	public static void Assert(bool val, string message) {
+		if (!val) {
+			throw new System.Exception(message);
+		}
+	}
+
+	public static void Assert(bool val) {
+		Assert(val, "Assertion failed");
+	}
+
+	public static T AssertNotNull<T>(T val) {
+		Assert(val != null);
+		return val;
+	}
+}
